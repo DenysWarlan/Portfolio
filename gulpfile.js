3f00d1e3fe6aec7b9aed3f28f05a4c0aff450162
Denys <denis.varlan95@gmail.com>
2018-06-11 23:56:46 +0200

    test

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber'),
+    browserSync = require('browser-sync');
+
+
+
+
+
+gulp.task('css', function () {
+
+    return gulp.src('./css/less/style.less')
+        .pipe(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(less({
+            compress: false
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions', "IE 9"],
+        }))
+        .pipe( gulp.dest('./css') )
+        .pipe( browserSync.stream());
+});
+
+gulp.task('server', function() {
+
+    // Not remove! This is config by denis
+    browserSync.init({
+        host: 'localhost',
+        open: 'external',
+        proxy: 'http://localhost/Github/IAI_test_2/index.html',
+        port: '8086'
+    });
+
+
+
+});
+
+gulp.task('gcmq', function () {
+    gulp.src('./css/***.css')
+        .pipe(gcmq())
+        .pipe(gulp.dest('css'));
+});
+
+
+
+
+
+
+gulp.task('watch', function() {
+    gulp.watch('./css/less/***.less', ['css']);
+    gulp.watch(['./**/*.html', './portfolio/js/common.js', './portfolio/php/send.php'], browserSync.reload);
+});
+
+gulp.task('default', ['css', 'server', 'watch']);
