fb90da26f15b855addca5dd9d63c224600aa5403
Denys <denis.varlan95@gmail.com>
2018-06-13 20:18:33 +0200

    contact footer form

--- js/common.js
+++ js/common.js
@@ -1,46 +1,263 @@
-window.onload = function() {
-    $('.slick-dots').wrap('<div class="container" style="display: flex; justify-content: flex-end;"></div>');
+(function ($) {
+    window.onload = function () {
+        $('.slick-dots').wrap('<div class="container" style="display: flex; justify-content: flex-end;"></div>');
 
-};
-
-
-$('.nav-item').on('click', function () {
-    var ui = $(this),
-        link = $('.nav-item');
-    ui.toggleClass('active');
-
-    if (ui.hasClass('active')) {
-        link.not(ui).removeClass('active');
-    }
-});
-$('#banner-title').mouseenter(function () {
-    $('#btn-more').addClass('hover');
-});
-$('#banner-title').mouseleave(function () {
-    $('#btn-more').removeClass('hover');
-});
-
-$(document).ready(function () {
-
-});
+    };
 
 
-$(document).ready(function () {
-    $('.banner-slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots:false
-                }
-            }]
+    $('.nav-item').on('click', function () {
+        var ui = $(this),
+            link = $('.nav-item');
+        ui.addClass('active');
+
+        if (ui.hasClass('active')) {
+            link.not(ui).removeClass('active');
+        }
     });
-})
-;
+    $('#banner-title').mouseenter(function () {
+        $('#btn-more').addClass('hover');
+    });
+    $('#banner-title').mouseleave(function () {
+        $('#btn-more').removeClass('hover');
+    });
+
+    $(document).ready(function () {
+
+    });
+
+
+    $(document).ready(function () {
+        $('.banner-slider').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: false
+                    }
+                }]
+        });
+    });
+
+
+    function initMap() {
+        var styledMapType = new google.maps.StyledMapType(
+            [
+                {elementType: 'geometry', stylers: [{color: '#d4d6d2'}]},
+                {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+                {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+                {
+                    featureType: 'administrative',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#d4d6d2'}]
+                },
+                {
+                    featureType: 'administrative.neighborhood',
+                    elementType: 'label.text',
+                    stylers: [{visibility: 'on'}]
+                },
+                {
+                    featureType: 'administrative.province',
+                    elementType: 'label.text',
+                    stylers: [{visibility: 'on'}]
+                },
+                {
+                    featureType: 'administrative.land_parcel',
+                    elementType: 'geometry.stroke',
+                    stylers: [
+                        {
+                            color: '#d4d6d2',
+                            visibility: 'on'
+                        }
+                    ]
+                },
+                {
+                    featureType: 'administrative.land_parcel',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#ae9e90'}]
+                },
+                {
+                    featureType: 'landscape',
+                    elementType: 'label.text',
+                    stylers: [{visibility: 'on'}]
+                },
+                {
+                    featureType: 'landscape.natural',
+                    elementType: 'geometry',
+                    stylers: [{color: '#d4d6d2'}]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'geometry',
+                    stylers: [{color: '#d4d6d2'}]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text',
+                    stylers: [{visibility: 'on'}]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#93817c'}]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.icon',
+                    stylers: [{visibility: 'off'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry.fill',
+                    stylers: [{color: '#a5b076'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#447530'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{color: '#f5f1e6'}]
+                },
+                {
+                    featureType: 'road.arterial',
+                    elementType: 'geometry',
+                    stylers: [{color: '#fdfcf8'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{color: '#f8c967'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#e9bc62'}]
+                },
+                {
+                    featureType: 'road.highway.controlled_access',
+                    elementType: 'geometry',
+                    stylers: [{color: '#e98d58'}]
+                },
+                {
+                    featureType: 'road.highway.controlled_access',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#db8555'}]
+                },
+                {
+                    featureType: 'road.local',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#806b63'}]
+                },
+                {
+                    featureType: 'transit.line',
+                    elementType: 'geometry',
+                    stylers: [{color: '#dfd2ae'}]
+                },
+                {
+                    featureType: 'transit.line',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#8f7d77'}]
+                },
+                {
+                    featureType: 'transit.line',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{color: '#ebe3cd'}]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'geometry',
+                    stylers: [{color: '#dfd2ae'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry.fill',
+                    stylers: [{color: '#b9d3c2'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#92998d'}]
+                }
+            ],
+            {name: 'Styled Map'});
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 23.03, lng: 72.5669967},
+            zoom: 12
+        });
+        map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
+
+    }
+
+    $('#form').validate({
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            mes: {
+                required: true
+            }
+
+        }
+    });
+
+    $(document).ready(function () {
+        $(function () {
+            $('#form').on('submit', function (e) {
+                if (!e.isDefaultPrevented()) {
+                    var url = "mail.php",
+                        formMes = $('.btn-form');
+
+                    $.ajax({
+                        type: "POST",
+                        url: url,
+                        data: $(this).serialize(),
+                        beforeSend: function () {
+                            formMes.next().text('Sending your letter...');
+                        },
+                        success: function (res) {
+                            if (res == 0) {
+                                formMes.next().text('Sent letter ');
+                                formMes.next().addClass('success');
+                                $('#form')[0].reset();
+                                setTimeout(resetSpan, 5000);
+                            } else {
+                                formMes.next().text('Something was wrong. Try later');
+                                formMes.next().addClass('error');
+                                setTimeout(resetSpan, 5000);
+                            }
+                        },
+                        error: function () {
+                            formMes.next().text('Something was wrong. Try later');
+                            formMes.next().addClass('error');
+                            setTimeout(resetSpan, 5000);
+
+                        }
+                    });
+                    return false;
+                }
+            })
+        });
+    });
+
+    function resetSpan() {
+        $('.btn-form').next().text(' ');
+    }
+
+
+
+})(jQuery);
--- mail.php
+++ mail.php
@@ -1,40 +1,37 @@
-<?php 
+<?php
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$name = $_POST['name'];
+$mes = $_POST['mes'];
+$email = $_POST['email'];
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'denis.varla95@gmail.com';  // Your login from the mail from which letters will be sent
+$mail->Password = 'Den4ik21';   // Your password from the mail from which letters will be sent
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+
+$mail->setFrom('denis.varla95@gmail.com');  // who will send the letter
+$mail->addAddress('ciluka@taylorventuresllc.com'); // who will receive the letter
+$mail->isHTML(true);
+
+$mail->Subject = 'Message Modest';
+$mail->Body    = 'User ' .$name . '<br>Message ' .$mes. '<br>Mael user ' .$email;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
-    echo 'Error';
+        $answer = '1';
 } else {
-    header('location: thank-you.html');
+    $answer = '0';
 }
+die($answer);
 ?>
